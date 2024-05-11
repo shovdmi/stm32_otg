3a5552a04e94cf72501e98da6c31cc4198e9c49f
shovdmi <shovdmi@yandex.ru>
2024-05-11 17:26:31 +0300

    WIP Receives requests/Sends responses

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include "stm32.h"
+#include <stddef.h>
+
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
 
 #if 0
 void _init(void)
@@ -11,6 +14,8 @@ void SystemInit()
 }
 #endif
 
+uint8_t usb_addr = 0;
+volatile uint8_t usb_addr_old = 0;
 
 void clock_initialization()
 {
@@ -87,28 +92,12 @@ void otg_detach()
 	//RCC->AHB2ENR &= ~RCC_AHB2ENR_OTGFSEN;
 }
 
-void otg_initialization()
+void otg_hw_reset()
 {
-	
-    RCC->AHB1ENR   |=  RCC_AHB1ENR_GPIOAEN;
-    GPIOA->MODER   |=  GPIO_MODER_MODE11_1  | GPIO_MODER_MODE12_1; // 10: Alternate function mode; 01: General purpose output mode
-    GPIOA->OTYPER  &= ~(GPIO_OTYPER_OT11  | GPIO_OTYPER_OT12);     //  0: Output push-pull (reset state)
-    GPIOA->OSPEEDR |=  (GPIO_OSPEEDER_OSPEEDR11  | GPIO_OSPEEDER_OSPEEDR12);   //  11: 
-    //GPIOA->PUPDR = GPIO_PUPDR_PUPD11_0 | GPIO_PUPDR_PUPD12_0;    // 01: Pull-up
-    GPIOA->PUPDR   &= ~(GPIO_PUPDR_PUPD11 | GPIO_PUPDR_PUPD12);    // 00: No pull-up, pull-down
-    // AF10 (OTG_FS)
-    GPIOA->AFR[1]  |= (10 << GPIO_AFRH_AFSEL11_Pos) | (10 << GPIO_AFRH_AFSEL12_Pos); 
-
-    //GPIOA->BSRR = GPIO_BSRR_BR4 | GPIO_BSRR_BR5;
-
-    //GPIOA->OTYPER = ~(GPIO_OTYPER_OT_4 | GPIO_OTYPER_OT5); //0: Output push-pull (reset state)
-    //GPIOA->MODER = GPIO_MODER_MODE4_0 | GPIO_MODER_MODE5_0;// 01: General purpose output mode
-    //GPIOA->BSRR = GPIO_BSRR_BR4 | GPIO_BSRR_BR5;
-    //GPIOA->PUPDR = GPIO_PUPDR_PUPD4_1 | GPIO_PUPDR_PUPD5_1;// 10: Pull-down
-    
-	RCC->AHB2ENR |= RCC_AHB2ENR_OTGFSEN;
-    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
-    __asm__("nop"); //  Delay after an RCC peripheral clock enabling
+	// PHY Clock restart
+    // see CSRST: Core soft reset. When you change the PHY, the corresponding clock for the PHY is selected and used in the
+    // PHY domain.Once a new clock is selected, the PHY domain has to be reset for proper operation.
+    *USB_OTG_IN_PCGCCTL = 0;
 
     // Select FS Embedded PHY
     // This bit is always 1 with read-only access. 
@@ -119,6 +108,8 @@ void otg_initialization()
         __asm__("nop");
     }
 
+    // All module state machines(except for the AHB slave unit) are reset to the Idle state, and 
+    // all the transmit FIFOs and the receive FIFO are flushed
     USB_OTG_FS->GRSTCTL |= USB_OTG_GRSTCTL_CSRST;
     while ((USB_OTG_FS->GRSTCTL & USB_OTG_GRSTCTL_CSRST) != 0) { 
         __asm__("nop");
@@ -129,20 +120,49 @@ void otg_initialization()
         __asm__("nop");
     }
 
+    // HCLK soft reset. The application uses this bit to flush the control logic in the AHB Clock domain.Only AHB Clock Domain pipelines are reset.
+    USB_OTG_FS->GRSTCTL |= USB_OTG_GRSTCTL_HSRST;
+    while ((USB_OTG_FS->GRSTCTL & USB_OTG_GRSTCTL_HSRST) != 0) { 
+        __asm__("nop");
+    }
+
+    while ((USB_OTG_FS->GRSTCTL & USB_OTG_GRSTCTL_AHBIDL) == 0) { // Indicates that the AHB master state machine is in the Idle condition.
+        __asm__("nop");
+    }
+
+}
+
+void otg_initialization()
+{
+    RCC->AHB1ENR   |=  RCC_AHB1ENR_GPIOAEN;
+    GPIOA->MODER   |=  GPIO_MODER_MODE11_1  | GPIO_MODER_MODE12_1; // 10: Alternate function mode; 01: General purpose output mode
+    GPIOA->OTYPER  &= ~(GPIO_OTYPER_OT11  | GPIO_OTYPER_OT12);     //  0: Output push-pull (reset state)
+    GPIOA->OSPEEDR |=  (GPIO_OSPEEDER_OSPEEDR11  | GPIO_OSPEEDER_OSPEEDR12);   //  11: 
+    GPIOA->PUPDR   &= ~(GPIO_PUPDR_PUPD11 | GPIO_PUPDR_PUPD12);    // 00: No pull-up, pull-down
+    GPIOA->AFR[1]  |=  (10 << GPIO_AFRH_AFSEL11_Pos) | (10 << GPIO_AFRH_AFSEL12_Pos); // AF10 (OTG_FS)
+
+    RCC->AHB2ENR |= RCC_AHB2ENR_OTGFSEN;
+    RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+    __asm__("nop"); //  Delay after an RCC peripheral clock enabling
+
+    otg_hw_reset();
+
 	USB_OTG_FS->GCCFG |= USB_OTG_GCCFG_PWRDWN;    // 1: Power down deactivated (“Transceiver active”)
-    
+
 
 	USB_OTG_FS->GUSBCFG  &= ~(USB_OTG_GUSBCFG_FDMOD | USB_OTG_GUSBCFG_FHMOD);
 	USB_OTG_FS->GUSBCFG  |=   USB_OTG_GUSBCFG_FDMOD;  // force OTG_FS core to work as a USB peripheral-only
 	// When HNP or SRP is enabled the VBUS sensing pin (PA9) pin should be connected to VBUS. 
 	// When HNP and SRP are both disabled, the VBUS sensing pin (PA9) should not be connected to VBUS.This pin can be can be used as GPIO.
-	USB_OTG_FS->GUSBCFG &= ~USB_OTG_GUSBCFG_HNPCAP;         // HNP capable bit (Host Negtiation Protocol)
-	USB_OTG_FS->GUSBCFG &= ~USB_OTG_GUSBCFG_SRPCAP;         // SRP capable bit 
+	USB_OTG_FS->GUSBCFG &= ~USB_OTG_GUSBCFG_HNPCAP;   // HNP capable bit (Host Negtiation Protocol)
+	USB_OTG_FS->GUSBCFG &= ~USB_OTG_GUSBCFG_SRPCAP;   // SRP capable bit 
 	
     while ((USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_CMOD) != 0) { // CMOD: Current mode of operation 0 : Device mode, 1 : Host mode
         __asm__("nop");
     }
 
+    // ---------
     {
         // Endpoint Init
     }
@@ -154,15 +174,9 @@ void otg_initialization()
     USB_OTG_FS->GCCFG    &= ~(USB_OTG_GCCFG_VBUSASEN | USB_OTG_GCCFG_VBUSBSEN);
     
 
-	// PHY Clock restart
-    *USB_OTG_IN_PCGCCTL = 0;
-
     USB_OTG_DEVICE->DCFG |=   0 << USB_OTG_DCFG_PFIVL_Pos;
     USB_OTG_DEVICE->DCFG |=   USB_OTG_DCFG_DSPD_Msk;  // 11: Full speed (USB 1.1 transceiver clock is 48 MHz)
 
-    {
-        // Flush FIFOs
-    }
     
 
     //-------------------------------------------------------------------------------------
@@ -203,6 +217,9 @@ void otg_initialization()
 	// unmask the following interrupts: USB reset, Enumeration done, Early suspend, USB suspend, SOF
 	USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_USBRST | USB_OTG_GINTMSK_ENUMDNEM | USB_OTG_GINTMSK_ESUSPM | USB_OTG_GINTMSK_USBSUSPM | USB_OTG_GINTMSK_SOFM;
 
+    NVIC_EnableIRQ(OTG_FS_IRQn);
+
+#if 0
 
 	// Wait for the USBRST interrupt. A reset has been detected on the USB that lasts for about 10 ms
 	while ((USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_USBRST) == 0) {
@@ -278,18 +295,208 @@ void otg_initialization()
         	__UNALIGNED_UINT32_WRITE(&buf[i], USBx_DFIFO(0U));
     	}
     	//ep->xfer_count += (RegVal & USB_OTG_GRXSTSP_BCNT) >> 4;
-    	USB_UNMASK_INTERRUPT(hpcd->Instance, USB_OTG_GINTSTS_RXFLVL);
+    	//USB_UNMASK_INTERRUPT(hpcd->Instance, USB_OTG_GINTSTS_RXFLVL);
     	
     	(void)len++;
 	}
+#endif
 }
+
+void  OTG_FS_WKUP_IRQHandler() 
+{
+}
+
+enum USB_Descriptor_Type_t {
+    DEVICE_DESCRIPTOR_TYPE        = 1,
+    CONFIGURATION_DESCRIPTOR_TYPE = 2,
+
+    INTERFACE_DESCRIPTOR_TYPE     = 4,
+    ENDPOINT_DESCRIPTOR_TYPE      = 5,
+
+    HID_DESCRIPTOR_TYPE           = 0x21,
+};
+
+#define EP0_MAX_PACKET_SIZE   64
+
+struct USB_Device_Descriptor_t {
+    uint8_t  bLength; // 0x12
+    uint8_t  bDescriptorType; // 1  Device Descriptor
+    uint16_t bcdUSB; // 0x00, 0x02 --  2.0
+
+    uint8_t  bDeviceClass; //
+    uint8_t  bDeviceSubClass; //
+    uint8_t  bDeviceProtocol; //
+
+    uint8_t  bMaxPacketSize0; // Maximum packet size for endpoint zero(only 8, 16, 32, or 64 are valid)
+    uint16_t idVendor; // VID Vendor ID
+    uint16_t idProduct; // PID Product ID
+    uint16_t bcdDevice; // Device release number (assigned by manufacturer).
+    uint8_t  iManufacturer; // Index of string descriptor describing manufacturer
+    uint8_t  iProduct; // Index of string descriptor describing product
+    uint8_t  iSerialNumber; // Index of string descriptor describing the device serial number
+    uint8_t bNumConfigurations; // Number of possible configurations. 
+};
+
+const struct USB_Device_Descriptor_t DeviceDescriptor __attribute__((aligned(16))) = {
+    .bLength = sizeof(struct USB_Device_Descriptor_t), // 0x12
+    .bDescriptorType = DEVICE_DESCRIPTOR_TYPE, // 1  Device Descriptor  
+    .bcdUSB = 0x200, // 0x00, 0x02 --  2.0
+
+    .bDeviceClass = 0,
+    //
+    .bDeviceSubClass = 0, //
+    .bDeviceProtocol = 0, //
+
+    .bMaxPacketSize0 = EP0_MAX_PACKET_SIZE,
+    // Maximum packet size for endpoint zero(only 8, 16, 32, or 64 are valid)
+    .idVendor = 0x03EB,
+    // VID Vendor ID
+    .idProduct = 2009,
+    // PID Product ID
+    .bcdDevice = 0x0001, // Device release number (assigned by manufacturer).
+    .iManufacturer = 1, // Index of string descriptor describing manufacturer
+    .iProduct = 2,
+    // Index of string descriptor describing product
+    .iSerialNumber = 3,
+    // Index of string descriptor describing the device serial number
+    .bNumConfigurations = 1, // Number of possible configurations. 
+};
+
+void  OTG_FS_IRQHandler() 
+{
+    uint32_t current_mode = 0;
+
+	if ((USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_USBRST) != 0) {
+    	USB_OTG_FS->GINTSTS = (USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_USBRST);
+    	USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_RXFLVLM;
+    	USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_NPTXFEM;
+    }
+
+	// Wait for the end of reset on the USB
+    if ((USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_ENUMDNE) != 0) {
+        USB_OTG_FS->GINTSTS = (USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_ENUMDNE);
+
+        //Read the OTG_FS_DSTS to determine the enumeration speed
+        current_mode = USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_CMOD; // CMOD: Current mode of operation 0 : Device mode, 1 : Host mode 
+        uint32_t device_status2 = USB_OTG_DEVICE->DSTS;
+        (void) device_status2;
+
+
+        // 22.17.5 Device programming model
+        // 1.  Set the NAK bit for all OUT endpoints. SNAK = 1 in OTG_FS_DOEPCTLx(for all OUT endpoints)
+        // FIXME--> USB_OTG_OUT_ENDPOINT0->DOEPCTL |= USB_OTG_DOEPCTL_CNAK;
+
+        // 2. Unmask the following interrupt bits –
+        // INEP0 = 1, OUTEP0 = 1 in OTG_FS_DAINTMSK(control 0 IN/OUT endpoint), 
+        USB_OTG_DEVICE->DAINTMSK |= (1 << USB_OTG_DAINTMSK_OEPM_Pos) | (1 << USB_OTG_DAINTMSK_IEPM_Pos); // EP0
+        // STUP = 1, XFRC = 1 in DOEPMSK; 
+        USB_OTG_DEVICE->DOEPMSK |= USB_OTG_DOEPMSK_STUPM | USB_OTG_DOEPMSK_XFRCM;
+        // XFRC = 1, TOC = 1 in DIEPMSK
+        USB_OTG_DEVICE->DIEPMSK |= USB_OTG_DIEPMSK_TOM | USB_OTG_DIEPMSK_XFRCM;
+        // 3.  Set up the Data FIFO RAM for each of the FIFOs. Program the OTG_FS_GRXFSIZ register
+        //RXFD: RxFIFO depth This value is in terms of 32 - bit words.  Minimum value is 16, Maximum value is 256
+        USB_OTG_FS->GRXFSIZ = 16; //0x200; // 0x200 is Reset value
+        //Program the OTG_FS_TX0FSIZ register (depending on the FIFO number chosen) to be able to transmit control IN data.
+        //At a minimum, this must be equal to 1 max packet size of control endpoint 0
+        USB_OTG_FS->DIEPTXF0_HNPTXFSIZ = 0x200; // 0x200 is Reset value. Bits 31:16 TX0FD: Endpoint 0 TxFIFO depth This value is in terms of 32 - bit words
+
+        //
+        // Endpoint activation
+        //
+        // Program the following fields in the endpoint-specific registers for control OUT endpoint 0 to receive a SETUP packet, STUPCNT = 3 in OTG_FS_DOEPTSIZ0(to receive up to 3 back - to - back SETUP packets)
+        USB_OTG_OUT_ENDPOINT0->DOEPTSIZ |= 3 << USB_OTG_DOEPTSIZ_STUPCNT_Pos;
+
+        USB_OTG_IN_ENDPOINT0->DIEPCTL  = (USB_OTG_IN_ENDPOINT0->DIEPCTL & ~USB_OTG_DIEPCTL_EPTYP_Msk) | (0 << USB_OTG_DIEPCTL_EPTYP_Pos); // 00 Control type
+        // The maximum packet size for a control endpoint depends on the enumeration speed.
+        //USB_OTG_IN_ENDPOINT0->DIEPCTL |= 3 << USB_OTG_DIEPCTL_MPSIZ_Pos; // 00: 64 bytes, 01: 32 bytes, 10: 16 bytes, 11: 8 bytes
+        USB_OTG_IN_ENDPOINT0->DIEPCTL |= (USB_OTG_IN_ENDPOINT0->DIEPCTL & ~USB_OTG_DIEPCTL_MPSIZ) | (0 << USB_OTG_DIEPCTL_MPSIZ_Pos); // 00: 64 bytes, 01: 32 bytes, 10: 16 bytes, 11: 8 bytes
+        //USB_OTG_IN_ENDPOINT0->DIEPCTL |= USB_OTG_DIEPCTL_USBAEP;         // USB active endpoint. Always is 1 for EP0
+        //USB_OTG_IN_ENDPOINT0->DIEPCTL |= USB_OTG_DIEPCTL_EPENA; // Endpoint enable
+
+        USB_OTG_OUT_ENDPOINT0->DOEPCTL  = (USB_OTG_OUT_ENDPOINT0->DOEPCTL & ~USB_OTG_DOEPCTL_EPTYP_Msk) | (0 << USB_OTG_DOEPCTL_EPTYP_Pos); // 00 Control type
+        USB_OTG_OUT_ENDPOINT0->DOEPCTL |= USB_OTG_DOEPCTL_CNAK; // Clear NAK. A write to this bit clears the NAK bit for the endpoint.
+        USB_OTG_OUT_ENDPOINT0->DOEPCTL |= 8 << USB_OTG_DOEPCTL_MPSIZ_Pos; // Maximum packet size. This value is in bytes
+        //USB_OTG_OUT_ENDPOINT0->DOEPCTL |= USB_OTG_DOEPCTL_USBAEP;         // USB active endpoint. Always is 1 for EP0
+        USB_OTG_OUT_ENDPOINT0->DOEPCTL |= USB_OTG_DOEPCTL_EPENA; // Endpoint enable
+    }
+
+    if ((USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_RXFLVL) != 0) {
+#define USB_OTG_GRXSTSP_FRMNUM_Pos                (21U)
+#define USB_OTG_GRXSTSP_FRMNUM_Msk                (0xFUL << USB_OTG_GRXSTSP_FRMNUM_Pos) /*!< 0x01E00000 */
+#define USB_OTG_GRXSTSP_FRMNUM                    USB_OTG_GRXSTSP_FRMNUM_Msk            /*!< Frame number*/
+
+        // On catching an RXFLVL interrupt (OTG_FS_GINTSTS register), the application must read the Receive status pop register(OTG_FS_GRXSTSP).
+        // A read to the GRXSTSP additionally pops the top data entry out of the RxFIFO
+        uint32_t rxstsp = USB_OTG_FS->GRXSTSP;
+        volatile uint32_t ep_num        = (rxstsp & USB_OTG_GRXSTSP_EPNUM)  >> USB_OTG_EPNUM_Pos;
+        volatile uint32_t data_pid      = (rxstsp & USB_OTG_GRXSTSP_DPID)   >> USB_OTG_DPID_Pos;
+        volatile uint32_t packet_status = (rxstsp & USB_OTG_GRXSTSP_PKTSTS) >> USB_OTG_PKTSTS_Pos;
+        volatile uint32_t frame_num     = (rxstsp & USB_OTG_GRXSTSP_FRMNUM) >> USB_OTG_FRMNUM_Pos;
+        volatile uint32_t bytes_count   = (rxstsp & USB_OTG_GRXSTSP_BCNT)   >> USB_OTG_BCNT_Pos;
+
+        current_mode = USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_CMOD; // CMOD: Current mode of operation 0 : Device mode, 1 : Host mode 
+
+        //    	USB_OTG_OUT_ENDPOINT0->DOEPTSIZ &= ~(USB_OTG_DOEPTSIZ_XFRSIZ);
+        //    	USB_OTG_OUT_ENDPOINT0->DOEPTSIZ &= ~(USB_OTG_DOEPTSIZ_PKTCNT);
+
+    	
+#define USBx_DFIFO(i)   *(__IO uint32_t *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_FIFO_BASE + ((i) * USB_OTG_FIFO_SIZE))
+        volatile uint8_t __attribute__((aligned(32))) buf[64];
+        for (int i = 0U; i < bytes_count; i += sizeof(uint32_t))
+        {
+            __UNALIGNED_UINT32_WRITE(&buf[i], USBx_DFIFO(0U));
+        }
+        
+        // 80 06 00 01 00 00 40 00
+        const uint8_t request1[] = { 0x80, 0x06, 0x00, 0x01, 0x00, 0x00, }; // 0x40, 0x00};
+        const uint8_t request2[] = { 0x00, 0x05 };
+        if (memcmp(&buf[0], request1, MIN(bytes_count, sizeof(request1))) == 0) {
+            size_t response_len = MIN(sizeof(DeviceDescriptor), buf[6]);
+
+            USB_OTG_IN_ENDPOINT0->DIEPTSIZ |= USB_OTG_DIEPTSIZ_PKTCNT | response_len;
+            USB_OTG_IN_ENDPOINT0->DIEPCTL  |= USB_OTG_DIEPCTL_EPENA | USB_OTG_DIEPCTL_CNAK; // Endpoint enable
+
+            size_t wc = (response_len + 3) / 4; // we must push this many words
+            for (size_t i = 0; i < wc; ++i) {
+                USBx_DFIFO(0U) = __UNALIGNED_UINT32_READ((uint8_t*)&DeviceDescriptor + 4 * i); // ok if we read over boundary
+            }
+        }
+        else if (memcmp(&buf[0], request2, MIN(bytes_count, sizeof(request2))) == 0) {
+            usb_addr = buf[2];
+            usb_addr_old = buf[2];
+            size_t response_len = 0; // ZLP
+            USB_OTG_IN_ENDPOINT0->DIEPTSIZ |= USB_OTG_DIEPTSIZ_PKTCNT | response_len;
+            USB_OTG_IN_ENDPOINT0->DIEPCTL  |= USB_OTG_DIEPCTL_EPENA | USB_OTG_DIEPCTL_CNAK; // Endpoint enable
+        }
+        else {
+            __asm__("bkpt 255");
+        }
+        USB_OTG_FS->GINTSTS = (USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_RXFLVL); // TODO: is it RW? correct?
+    	USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_RXFLVLM;
+    }
+    if ((USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_NPTXFE) != 0) {
+        if (usb_addr != 0) {
+            USB_OTG_DEVICE->DCFG = usb_addr << USB_OTG_DCFG_DAD_Pos;
+            usb_addr = 0;
+        }
+        USB_OTG_FS->GINTSTS = (USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_NPTXFE); // TODO: is it RW? correct?
+    	USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_NPTXFEM;
+    }
+
+    if (USB_OTG_IN_ENDPOINT0->DIEPINT & USB_OTG_DIEPINT_XFRC){
+        //FIXME: Why??
+        USB_OTG_OUT_ENDPOINT0->DOEPCTL |= USB_OTG_DOEPCTL_CNAK; // Clear NAK. A write to this bit clears the NAK bit for the endpoint.
+
+        USB_OTG_IN_ENDPOINT0->DIEPINT = USB_OTG_DIEPINT_XFRC;
+    }
+}
+
 
 int main(void)
 {
-	while(1)
-	{
-		clock_initialization();
-    	otg_detach();
-		otg_initialization();
+    clock_initialization();
+    otg_detach();
+    otg_initialization();
+    while(1) {
 	}
 }
